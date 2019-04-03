fae605b21cce0bbb18c73e0e320023e8f2f12638
Anna Lornitzo <lornitzoa@gmail.com>
2019-04-03 11:29:23 -0400

    testing login to just print User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,14 +45,15 @@ class UsersController < ApplicationController
   # LOGIN /users/login
   def login
     puts '======== logging in ==========='
-    user = User.find_by(username: params[:user][:username])
-    puts user
-    if user && user.authenticate(params[:user][:password])
-      token = create_token(user.id, user.username)
-      render json: {status: 200, token: token, user: user}
-    else
-      render json: {status: 401, message: 'unauthorized'}
-    end
+    puts User
+    # user = User.find_by(username: params[:user][:username])
+    # puts user
+    # if user && user.authenticate(params[:user][:password])
+    #   token = create_token(user.id, user.username)
+    #   render json: {status: 200, token: token, user: user}
+    # else
+    #   render json: {status: 401, message: 'unauthorized'}
+    # end
   end
 
   private
