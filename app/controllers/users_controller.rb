c4a61403518ff5893b134b3820066dd1229d9cb1
Anna Lornitzo <lornitzoa@gmail.com>
2019-04-12 15:14:36 -0400

    add index to exceptions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-  before_action :authenticate_token, except: [:login, :register]
+  before_action :authenticate_token, except: [:login, :register, :index]
   before_action :authorize_user, except: [:login, :register, :index]
   # before_action :authenticate_user!, except: [:new, :register]
 
